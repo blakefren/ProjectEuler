371ca38edc4e9a3b1938af146268ced06ada1260
Blake French <blakefren@gmail.com>
2020-02-17 14:45:05 -0800

    Update prob_1.py
    fixed minor mistake

--- python/prob_1.py
+++ python/prob_1.py
@@ -4,8 +4,8 @@ def prob_1():
     '''
     
     sum = 0
-    for i in xrange(1000):
-        if i%3==0 or i%5==0:
+    for i in range(1000):
+        if i % 3==0 or i % 5==0:
             sum += i
     return sum
 
