d899ac557141dcfbf29d009ae7918e969c1a4195
Blake French <blakefren@gmail.com>
2020-02-17 15:27:18 -0800

    add prob 3

---
+++ python/prob_0003.py
@@ -0,0 +1,25 @@
+def prob_3():
+    '''
+    Answer: 
+    '''
+    n = 600851475143
+    largest_prime = 2
+    i = largest_prime
+    
+    while i * i < n:
+
+        # Reduce n by this prime as many times as possible.
+        while n % i == 0:
+            n /= i
+
+        # Find the next largest factor (should be a prime number)
+        while n % i != 0:
+            i += 1
+
+        largest_prime = i
+    
+    return largest_prime
+
+
+if __name__ == '__main__':
+    print(prob_3())
