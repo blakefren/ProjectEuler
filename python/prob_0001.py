16041add5093f7b0363b120666c29ff5b24f9dd9
Blake French <blakefren@gmail.com>
2020-02-17 17:03:43 -0800

    slight rework

--- python/prob_0001.py
+++ python/prob_0001.py
@@ -1,14 +1,13 @@
-def prob_1():
-    '''
-    Answer: 233168
-    '''
+def prob_1(n):
+    # O(n) solution.
     
     sum = 0
-    for i in range(1000):
+    for i in range(n):
         if i % 3==0 or i % 5==0:
             sum += i
     return sum
 
 
 if __name__ == '__main__':
-    print(prob_1())
+    # Answer: 233168
+    print(prob_1(1000))
