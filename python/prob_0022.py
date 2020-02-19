39a7aee827daa2280e490ef31923a7b461508ecf
Blake French <blakefren@gmail.com>
2020-02-19 10:23:13 -0800

    Update prob_0022.py

--- python/prob_0022.py
+++ python/prob_0022.py
@@ -1,20 +1,21 @@
 import os, sys
 
 def prob_22(filename):
-    # O(n) solution.
+    # O(n*log(n) + c) solution, where n is the number of names
+    # and c is the total number of characters in the file.
     
     raw_names = None
     filename = os.path.join(sys.path[0], filename)
     with open(filename, 'r') as f:
         raw_names = f.read()
     
-    names = raw_names.split(',')
-    names = sorted([name[1:len(name)-1] for name in names])
+    names = raw_names.split(',')  # O(c)
+    names = sorted([name[1:len(name)-1] for name in names])  # O(n*log(n))
 
     base = ord('A') - 1
     total_score = 0
 
-    for i, n in enumerate(names):
+    for i, n in enumerate(names):  # O(n*avg_len(n)) loop.
         name_score = 0
         for char in n:
             name_score += ord(char) - base
